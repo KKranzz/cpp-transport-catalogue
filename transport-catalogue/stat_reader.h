c012055b9342b46b1f3ca1293cccc180ec5a885c
KKranzz <117629281+KKranzz@users.noreply.github.com>
2024-02-14 17:43:16 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,13 @@
-// место для вашего кода
+#pragma once
+
+#include <iosfwd>
+#include <string_view>
+#include <unordered_set>
+#include <iomanip>
+
+#include "transport_catalogue.h"
+
+namespace statreader {
+    void ParseAndPrintStat(const transport_catalogue::processing::TransportCatalogue& tansport_catalogue, std::string_view request,
+        std::ostream& output);
+}
