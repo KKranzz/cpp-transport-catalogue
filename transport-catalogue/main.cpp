bc3903a58b37d11d4383baa9e125a1a58628bdd4
KKranzz <117629281+KKranzz@users.noreply.github.com>
2024-02-14 23:41:37 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -10,25 +10,11 @@ using namespace std;
 int main() {
     transport_catalogue::processing::TransportCatalogue catalogue;
     fstream file("C:\\Users\\1655082\\Desktop\\testdata\\t1i.txt");
-    
-    int base_request_count;
-    file >> base_request_count >> ws;
-   
-    {
-        inreader::InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
-            string line;
-            getline(file, line);
-            reader.ParseLine(line);
-        }
-        reader.ApplyCommands(catalogue);
-    }
+    inreader::InputReader reader(&file);
+    reader.StartParcing(catalogue);
+
     ofstream ifile("C:\\Users\\1655082\\Desktop\\testdata\\t1o.txt");
-    int stat_request_count;
-    file >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
-        string line;
-        getline(file, line);
-        statreader::ParseAndPrintStat(catalogue, line, ifile);
-    }
+    statreader::StatReader sreader(&ifile, &file);
+    sreader.StartRequestParcing(catalogue);
+
 }
