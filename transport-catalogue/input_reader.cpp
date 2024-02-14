9793a4ae19a64d11f3ebf7b8f4b3114a3d375325
KKranzz <117629281+KKranzz@users.noreply.github.com>
2024-02-14 17:41:17 +0300

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,1 +1,117 @@
-// место для вашего кода
+#include "input_reader.h"
+
+/*
+ Парсит строку в структуру CommandDescription и сохраняет результат в commands_
+ */ 
+std::vector<std::string> ParserByParameters(std::string_view string_line, char type_delimeter)
+{
+    std::vector <std::string> res;
+  
+        res.reserve(std::count(string_line.begin(), string_line.end(), type_delimeter));
+        std::string parameter;
+        for (auto symbol : string_line)
+        {
+            if (symbol == type_delimeter)
+            {
+                parameter.erase(parameter.begin(), parameter.begin() + parameter.find_first_not_of(" "));
+                parameter.erase(parameter.begin() + parameter.find_last_not_of(" ") + 1, parameter.end());
+
+                res.push_back(std::move(parameter));
+                parameter.clear();
+
+            }
+            else
+            {
+                parameter += symbol;
+            }
+        }
+
+        if (!parameter.empty()) 
+        {
+            parameter.erase(parameter.begin(), parameter.begin() + parameter.find_first_not_of(" "));
+            parameter.erase(parameter.begin() + parameter.find_last_not_of(" ")+1, parameter.end());
+            res.push_back(parameter);
+        }
+
+    return res;
+}
+
+void inreader::InputReader::ParseLine(std::string_view line) 
+{
+    CommandDescription res;
+    if (line.find(":") != std::string::npos) // add operation
+    {
+        if (line[0] == 'B')
+        {
+            if (line.find(">")!=std::string::npos) {
+                res.command = "ACBus";
+            }
+            else
+            {
+                res.command = "ALBus";
+            }
+           
+            res.id = std::move(std::string(line.begin() + line.find_first_of(" ") + 1, 
+                line.begin() + line.find_first_of(":")));
+            res.description = std::string(line.begin() + line.find_first_of(":") + 1, line.end());
+
+        }
+        else 
+        {
+            res.command = "AStop";
+            res.id = std::move(std::string(line.begin() + line.find_first_of(" ") + 1,
+                line.begin() + line.find_first_of(":")));
+            res.description = std::string(line.begin() + line.find_first_of(":")+1, line.end());
+        }
+   
+    }
+    else if(line[0]=='B') // bus info
+    {
+        res.command = "GBus";
+        res.id = std::move(std::string(line.begin() + line.find_first_of(" ") + 1,
+            line.begin() + line.find_first_of(":")));
+        
+    }
+    else 
+    {
+        return;
+    }
+    commands_.push_back(res);
+
+}
+
+/**
+ * Наполняет данными транспортный справочник, используя команды из commands_
+ */
+void inreader::InputReader::ApplyCommands(transport_catalogue::processing::TransportCatalogue& catalogue)
+{
+    std::sort(commands_.begin(), commands_.end(), [](const auto& lhs, const auto& rhs)
+     {
+          return lhs.command == "AStop" && rhs.command != "AStop";
+     });
+
+    for (auto& command : commands_) 
+    {
+        if (command.command == "AStop")
+        {
+            std::vector<std::string> params = std::move(ParserByParameters(command.description, ','));
+            geo_calc::Coordinates coord(std::stod(params[0]), std::stod(params[1]));
+            catalogue.AddStop(command.id, coord);
+        }
+        else if (command.command == "ACBus") 
+        {
+            std::vector<std::string> params = std::move(ParserByParameters(command.description, '>'));
+            catalogue.AddBus(command.id, std::move(params));
+        }
+        else if (command.command == "ALBus")
+        {
+            std::vector<std::string> params = std::move(ParserByParameters(command.description, '-'));
+            std::vector <std::string> second_part(params.begin(), params.end()-1);
+            for (int i = second_part.size() - 1; i >= 0; --i) 
+            {
+                params.push_back(std::move(second_part[i]));
+            }
+            catalogue.AddBus(command.id, std::move(params));
+        }
+    }
+}
