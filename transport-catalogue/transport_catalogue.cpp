4a9b0ee389c06d1ea798232bfe1ffef47f98e881
KKranzz <117629281+KKranzz@users.noreply.github.com>
2024-02-15 22:20:19 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,13 +1,13 @@
 #include "transport_catalogue.h"
 
 
-void transport_catalogue::processing::TransportCatalogue::AddStop(std::string name, geo_calc::Coordinates coord)
+void transport_catalogue::processing::TransportCatalogue::AddStop(std::string&& name, geo_calc::Coordinates coord)
 {
 	stops_.push_back({ std::move(name), std::move(coord) });
 	stopname_to_stops_[stops_.back().name_] = &stops_.back();
 }
 
-const transport_catalogue::identity::Stop* transport_catalogue::processing::TransportCatalogue::FindStop(std::string name_stop)
+const transport_catalogue::identity::Stop* transport_catalogue::processing::TransportCatalogue::FindStop(const std::string& name_stop)
 const {
 	if (stopname_to_stops_.find(name_stop) != stopname_to_stops_.end()) {
 		return stopname_to_stops_.at(name_stop);
@@ -15,7 +15,7 @@ const {
 	return nullptr;
 }
 
-void transport_catalogue::processing::TransportCatalogue::AddBus(std::string name, std::vector<std::string> stopnames)
+void transport_catalogue::processing::TransportCatalogue::AddBus(const std::string& name, std::vector<std::string>& stopnames)
 {
 	transport_catalogue::identity::Bus new_bus;
 	new_bus.name_ = name;
@@ -32,7 +32,7 @@ void transport_catalogue::processing::TransportCatalogue::AddBus(std::string nam
 	}
 }
 
-const transport_catalogue::identity::Bus* transport_catalogue::processing::TransportCatalogue::FindBus(std::string name_bus)
+const transport_catalogue::identity::Bus* transport_catalogue::processing::TransportCatalogue::FindBus(const std::string& name_bus)
 const {
 	if (busname_to_bus_.find(name_bus) != busname_to_bus_.end()) {
 		return busname_to_bus_.at(name_bus);
