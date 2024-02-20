b6a97a3c8ecf7d7e8c4eb698223d7d2e4228fa10
KKranzz <117629281+KKranzz@users.noreply.github.com>
2024-02-20 20:13:53 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -24,7 +24,7 @@ namespace transport_catalogue {
 		struct Bus
 		{
 			std::string name_;
-			std::vector<const identity::Stop*> stops_;
+			std::vector<identity::Stop*> stops_;
 		};
 
 	}
@@ -32,6 +32,14 @@ namespace transport_catalogue {
 		class TransportCatalogue {
 			// Реализуйте класс самостоятельно
 		public:
+			struct TransportHasher 
+			{
+				size_t operator()(std::pair <identity::Stop*, identity::Stop*> pair_pointer) const  {
+					return hasher(pair_pointer.first) + hasher(pair_pointer.second) * 37;
+				}
+				std::hash<const void*> hasher;
+			};
+		
 			void AddStop(std::string&& name, geo_calc::Coordinates coord);
 
 			const identity::Stop* FindStop(const std::string& name_stop) const;
@@ -40,11 +48,16 @@ namespace transport_catalogue {
 
 			const identity::Bus* FindBus(const std::string& name_bus) const;
 
+			void AddDistance(std::string&& first, std::string&& second, int dist);
+
+			int FindDist(identity::Stop* stop1, identity::Stop* stop2);
+
 		private:
 			std::deque<identity::Stop> stops_;
 			std::unordered_map<std::string_view, identity::Stop*> stopname_to_stops_;
 			std::deque<identity::Bus> buses_;
 			std::unordered_map <std::string_view, const identity::Bus*> busname_to_bus_;
+			std::unordered_map <std::pair<identity::Stop*, identity::Stop*>, int, TransportHasher> dist_data_;
 		};
 	}
 
