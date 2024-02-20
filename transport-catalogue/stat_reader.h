6f58f7e6490d5c833eedc7bc0d0b5ed2b9896b96
KKranzz <117629281+KKranzz@users.noreply.github.com>
2024-02-20 20:13:01 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -15,8 +15,8 @@ namespace statreader {
     class StatReader {
     public:
         StatReader(std::ostream* os, std::istream* is) : os_(os), is_(is) {}
-        void StartRequestParcing(const transport_catalogue::processing::TransportCatalogue& tansport_catalogue);
-        void ParseAndPrintStat(const transport_catalogue::processing::TransportCatalogue& tansport_catalogue, std::string_view request);
+        void StartRequestParcing(transport_catalogue::processing::TransportCatalogue& tansport_catalogue);
+        void ParseAndPrintStat(transport_catalogue::processing::TransportCatalogue& tansport_catalogue, std::string_view request);
     private:
         std::ostream* os_ = nullptr;
         std::istream* is_ = nullptr;
