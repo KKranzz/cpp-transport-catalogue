5d14d911d1e992bf5a1615881bb0f74bc959a7f9
KKranzz <117629281+KKranzz@users.noreply.github.com>
2024-04-08 20:30:51 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -2,19 +2,13 @@
 #include <string>
 #include <fstream>
 
-#include "input_reader.h"
-#include "stat_reader.h"
+#include "json_reader.h"
 
 using namespace std;
 
 int main() {
-    transport_catalogue::processing::TransportCatalogue catalogue;
-    fstream file("C:\\Users\\1655082\\Desktop\\testdata\\t1i.txt");
-    inreader::InputReader reader(&file);
-    reader.StartParcing(catalogue);
-
-    ofstream ifile("C:\\Users\\1655082\\Desktop\\testdata\\t1o.txt");
-    statreader::StatReader sreader(&ifile, &file);
-    sreader.StartRequestParcing(catalogue);
-
+   transport_catalogue::processing::TransportCatalogue catalogue;
+   JsonReader jreader;
+   jreader.JsonIn(catalogue, std::cin);
+   jreader.JsonOut(catalogue, std::out);
 }
